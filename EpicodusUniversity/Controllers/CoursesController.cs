47116f407cd01a8174132dbd5cb6ec3e6847fddd
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-24 14:06:45 -0700

    :sparkles: added controller routes and stuff
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

---
+++ EpicodusUniversity/Controllers/CoursesController.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using University.Models;
+using System.Linq;
+
+namespace University.Controllers
+{
+  public class CoursesController : Controller
+  {
+    private readonly UniversityContext _db;
+
+    public CoursesController(UniversityContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Course> model = _db.Courses.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisCourse = _db.Courses
+          .Include(course => course.JoinEntities)
+          .ThenInclude(join => join.Student)
+          .FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Entry(course).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+
+
+  }
+}
---
+++ EpicodusUniversity/Models/UniversityContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace University.Models
+{
+  public class UniversityContext : DbContext
+  {
+    public virtual DbSet<Course> Courses { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<CourseStudent> CourseStudent { get; set; }
+    public UniversityContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
