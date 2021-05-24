db8ad8d167ce1c355db3a80303ae944a18a06b88
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-24 11:54:31 -0700

    :sparkles: add some boilerplate and getting going
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

---
+++ EpicodusUniversity/Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using EpicodusUniversity.Models;
+using System.Linq;
+
+namespace University.Controllers
+{
+  public class StudentsController : Controller
+  {
+
+  }
+}
---
+++ EpicodusUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace University.Models
+{
+  public class Course
+  {
+    public Course()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+    }
+    public int CourseId { get; set; }
+    public string CourseTitle { get; set; }
+    public int CourseNum { get; set; }
+    public virtual ICollection<CourseStudent> JoinEntities { get; set; }
+  }
+}
