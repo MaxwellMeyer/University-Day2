60e55c02b58c240e613c0e664c1b35274e90f4ce
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-24 15:06:12 -0700

    :sparkles: got it to work!
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- EpicodusUniversity/Startup.cs
+++ EpicodusUniversity/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using ToDoList.Models;
+using University.Models;
 
 namespace University
 {
@@ -24,7 +24,7 @@ namespace University
     {
       services.AddMvc();
       services.AddEntityFrameworkMySql()
-        .AddDbContext<ToDoListContext>(options => options
+        .AddDbContext<UniversityContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
